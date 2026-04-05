b56bb74f8c86536f0c05ad782c211bf041f14962
oshada <oshadaov@gmail.com>
2026-04-05 14:32:19 +0530

    update corsconfig.java

--- src/main/java/com/ethi/village/config/CorsConfig.java
+++ src/main/java/com/ethi/village/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://ethi-village-git-dev-oshadaovs-projects.vercel.app",
+                "http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
